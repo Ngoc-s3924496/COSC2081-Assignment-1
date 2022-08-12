3fcb4ad45fe068251cf8055a3989c4da37fd8bf7
Thach-s3927021 <93109265+NgoTranBaoThach@users.noreply.github.com>
2022-08-12 14:12:33 +0700

    Modify Customer, Member Class

---
+++ FinalProject/Product/ProductList.java
@@ -0,0 +1,95 @@
+package FinalProject.Product;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class ProductList
+{
+    private static final String delimiter = ",";
+    private List<List<String>> productList = new ArrayList<>();
+
+    private List<List<String>> loadProductList() throws IOException
+    {
+        String filePath = "src\\Product.csv";
+        return ProductList.read(filePath);
+    }
+
+    public ProductList() throws IOException
+    {
+        this.productList = loadProductList();
+    }
+
+    public List<List<String>> getProductList()
+    {
+        return productList;
+    }
+
+
+    private static List<List<String>> read(String csvFile) throws IOException
+    {
+        List<List<String>> finalArr = new ArrayList<>();
+        File file = new File(csvFile);
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String line;
+        List<String> arr;
+        while ((line = bufferedReader.readLine()) != null)
+        {
+            arr = List.of(line.split(delimiter));
+            finalArr.add(arr);
+        }
+        bufferedReader.close();
+        return finalArr;
+    }
+
+    public void displayProductList()
+    {
+        for (List<String> strings : this.productList)
+        {
+            System.out.println(strings);
+        }
+    }
+
+    public void addNewProduct(Product productInput)
+    {
+        Product product = new Product();
+        List<String> newProduct = productInput.convertToListString();
+        this.productList.add(newProduct);
+    }
+
+    public void removeProduct(String productID)
+    {
+        List<List<String>> productList = getProductList();
+        for (List<String> products : productList)
+        {
+            if (Objects.equals(products.get(0), productID))
+            {
+                productList.remove(products);
+                break;
+            }
+        }
+    }
+
+    public void saveToCSV() throws IOException
+    {
+        File fileSrc = new File("src\\Product.csv");
+        File fileOut = new File("out\\production\\untitled\\Product.csv");
+        FileWriter fileWriterSrc = new FileWriter(fileSrc);
+        FileWriter fileWriterOut = new FileWriter(fileOut);
+        for (List<String> strings : this.productList)
+        {
+            String newProductString = String.valueOf(strings);
+
+            newProductString = newProductString.replace("[", "");
+            newProductString = newProductString.replace("]", "");
+            newProductString = newProductString.replace(", ", ",");
+            fileWriterSrc.write(newProductString + "\n");
+            fileWriterOut.write(newProductString + "\n");
+        }
+        fileWriterSrc.close();
+        fileWriterOut.close();
+    }
+}
+
---
+++ FinalProject/Product/Validate.java
@@ -0,0 +1,48 @@
+package FinalProject.Product;
+
+import java.util.List;
+
+public class Validate
+{
+    public Validate() {}
+    public boolean checkCategory(String input)
+    {
+        boolean categoryCheck = false;
+        Category category = new Category();
+        List<String> availableCategory = category.getAvailableCategory();
+        for (String s : availableCategory)
+        {
+            if (input.matches(s))
+            {
+                categoryCheck = true;
+                break;
+            }
+        }
+        return categoryCheck;
+    }
+
+    public boolean checkUnit(String input)
+    {
+        boolean unitCheck = false;
+        String[] unitList = {"item", "box", "tube", "packet", "bottle", "can"};
+        for (String s : unitList)
+        {
+            if (input.matches(s))
+            {
+                unitCheck = true;
+                break;
+            }
+        }
+        return unitCheck;
+    }
+
+    public boolean checkQuantity(String input)
+    {
+        String numericList = "[0-9]*";
+        return input.matches(numericList);
+    }
+    public boolean checkPrice(String input) {
+        String numericList = "[0-9]*";
+        return input.matches(numericList);
+    }
+}
